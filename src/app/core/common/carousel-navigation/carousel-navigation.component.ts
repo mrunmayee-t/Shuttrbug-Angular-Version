4dbe0595a21a9b0426ac3b5548806ed4ebc22509
Mrunmayee Talwalkar <mrunmayee.talwalkar@ad.infosys.com>
2020-08-05 23:55:36 +0530

    ui banner changes

--- src/app/core/common/carousel-navigation/carousel-navigation.component.ts
+++ src/app/core/common/carousel-navigation/carousel-navigation.component.ts
@@ -23,6 +23,42 @@ export class CarouselNavigationComponent implements OnInit {
     {
       id: 2,
       src: "../../../../assets/images/banner_3.jpg"
+    },
+    {
+      id: 3,
+      src: "../../../../assets/images/banner_1.jpg"
+    },
+    {
+      id: 4,
+      src: "../../../../assets/images/banner_4.jpg"
+    },
+    {
+      id: 5,
+      src: "../../../../assets/images/banner_5.jpg"
+    },
+    {
+      id: 6,
+      src: "../../../../assets/images/banner_6.jfif"
+    },
+    {
+      id: 7,
+      src: "../../../../assets/images/banner_7.jfif"
+    },
+    {
+      id: 8,
+      src: "../../../../assets/images/banner_8.jpg"
+    },
+    {
+      id: 9,
+      src: "../../../../assets/images/banner_9.jpg"
+    },
+    {
+      id: 10,
+      src: "../../../../assets/images/banner_10.jpg"
+    },
+    {
+      id: 11,
+      src: "../../../../assets/images/banner_11.jpg"
     }];
   }
 
